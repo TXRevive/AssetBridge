561cd49528b45bb85af05d0804bf2ea0ced97c23
Kaveman <55042475+Kaveinator@users.noreply.github.com>
2025-03-06 01:13:49 -0800

    Bridge PoC

---
+++ Scripts/NewBehaviourScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TXR.Tests {
+    public partial class NewBehaviourScript : MonoBehaviour {
+        [SerializeField] string PublicCartographerField;
+    }
+}
